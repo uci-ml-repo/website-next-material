53077712027d3ca63a00daece3c30977c89726f6
Andrew Wang <awwang3@uci.edu>
2026-06-29 00:22:31 -0700

    chore: init

---
+++ apps/website/src/app/layout.tsx
@@ -0,0 +1,37 @@
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v16-appRouter";
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import theme from "@/theme";
+import "./globals.css";
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto",
+});
+
+export const metadata: Metadata = {
+  title: "UCI Machine Learning Repository",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={roboto.variable}>
+      <body>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {children}
+          </ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
+    </html>
+  );
+}
---
+++ apps/website/src/app/page.tsx
@@ -0,0 +1,5 @@
+import Typography from "@mui/material/Typography";
+
+export default function Home() {
+  return <Typography>Page</Typography>;
+}
